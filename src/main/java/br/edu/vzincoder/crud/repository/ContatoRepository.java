4741e1dc748632b2ff6311db39d6c5f60fca975a
VzinCoder <contato.viniciusp.b@gmail.com>
2024-05-16 17:16:05 -0300

    adicionado novos metodos de busca no repository

--- src/main/java/br/edu/vzincoder/crud/repository/ContatoRepository.java
+++ src/main/java/br/edu/vzincoder/crud/repository/ContatoRepository.java
@@ -3,7 +3,11 @@ package br.edu.vzincoder.crud.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.vzincoder.crud.model.Contato;
+import java.util.List;
+
 
 public interface ContatoRepository extends JpaRepository<Contato, Integer> {
     
+    List<Contato> findByNome(String nome);
+    List<Contato> findByNumero(String numero);
 }
